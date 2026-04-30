24a30ff33797fdb23a84b9aad16e8459b6afbccb
Susanta Das <dassushant590@gmail.com>
2026-04-30 09:00:41 +0530

    Add CORS options for client URLs

--- server/index.ts
+++ server/index.ts
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 3000;
 const corsOptions = {
     origin:[
         "http://localhost:5173",
+        "https://foodapp-client-6dfq.vercel.app",
         process.env.CLIENT_URL
     ],
     credentials:true,
